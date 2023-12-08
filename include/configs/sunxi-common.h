85643542e1799e44e65acd9dfb28fdd7877549df
hejiawencc <hjw0415@outlook.com>
2023-12-08 14:38:21 +0800

    Fix emmc boot failed from script

--- include/configs/sunxi-common.h
+++ include/configs/sunxi-common.h
@@ -337,14 +337,11 @@
 #define BOOTENV_DEV_MMC_AUTO(devtypeu, devtypel, instance)		\
 	BOOTENV_DEV_MMC(MMC, mmc, 0)					\
 	BOOTENV_DEV_MMC(MMC, mmc, 1)					\
+	BOOTENV_DEV_MMC(MMC, mmc, 2)					\
 	"bootcmd_mmc_auto="						\
-		"if test ${mmc_bootdev} -eq 1; then "			\
-			"run bootcmd_mmc1; "				\
-			"run bootcmd_mmc0; "				\
-		"elif test ${mmc_bootdev} -eq 0; then "			\
 			"run bootcmd_mmc0; "				\
 			"run bootcmd_mmc1; "				\
-		"fi\0"
+			"run bootcmd_mmc2\0"
 
 #define BOOTENV_DEV_NAME_MMC_AUTO(devtypeu, devtypel, instance) \
 	"mmc_auto "
